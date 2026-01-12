49197def09e12e566fa5d61e5322f32886b62bab
Mario <mario@krikstudio.com>
2026-01-12 14:25:20 +0100

    Added comments to clean.js script

--- prisma/clean.js
+++ prisma/clean.js
@@ -11,8 +11,11 @@ const prisma = new PrismaClient({ adapter });
 async function main() {
   console.log("Cleaning database...");
 
+  // Deleting all rows from the Step table
   await prisma.step.deleteMany();
+  // Deleting all rows from the Ingredient table
   await prisma.ingredient.deleteMany();
+  // Deleting all rows from the Recipe table
   await prisma.recipe.deleteMany();
 
   console.log("Database cleaned.");
