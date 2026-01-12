6f81eea99650a467bace22d767162c6cd79036cb
Mario <mario@krikstudio.com>
2026-01-12 14:28:11 +0100

    Meal type enum

---
+++ src/lib/enums/mealType.ts
@@ -0,0 +1,10 @@
+import { MealType } from "@prisma/client";
+
+export const mealTypeLabels: Record<MealType, string> = {
+  desert: "Desert",
+  salata: "Salata",
+  juha: "Juha",
+  varivo: "Varivo",
+  meso: "Meso",
+  tjestenina: "Tjestenina",
+};
