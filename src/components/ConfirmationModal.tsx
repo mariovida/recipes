c8331a6714e49b7b464caee07d1eb16e739c80bb
Mario <mario@krikstudio.com>
2026-01-14 14:37:30 +0100

    Confirmation modal component

---
+++ src/components/ConfirmationModal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { BadgeCheck } from "lucide-react";
+import "@/styles/components/confirmationModal.scss";
+
+type ConfirmationModalProps = {
+  open: boolean;
+  onClose: (open: boolean) => void;
+  title: string;
+  description: string;
+  onConfirm?: () => void;
+};
+
+export default function ConfirmationModal({
+  open,
+  onClose,
+  title,
+  description,
+  onConfirm,
+}: ConfirmationModalProps) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="modal-overlay" />
+        <Dialog.Content className="modal-content">
+          <Dialog.Title>
+            <span>
+              <BadgeCheck size={22} color="#f0960f" />
+            </span>
+            {title}
+          </Dialog.Title>
+          <Dialog.Description className="modal-description">
+            {description}
+          </Dialog.Description>
+
+          <button
+            className="btn-primary"
+            onClick={() => {
+              onClose(false);
+              onConfirm?.();
+            }}
+          >
+            Zatvori
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
