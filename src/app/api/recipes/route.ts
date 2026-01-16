503cc68ecb127a48f35c61520c94361083f3277c
Mario <mario@krikstudio.com>
2026-01-16 11:57:01 +0100

    Small text change

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     const recipe = await createRecipe(data);
     return NextResponse.json(recipe, { status: 201 });
   } catch (err: unknown) {
-    if (err instanceof Error && err.message.includes("Slug already exists")) {
+    if (err instanceof Error && err.message.includes("Slug already exists.")) {
       return NextResponse.json(
         { error: "Slug already exists." },
         { status: 409 }
